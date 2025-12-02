923451fbf58b4354b58cf477856e89b8c1b4d8cf
ounis <ounis.jihene24@gmail.com>
2025-12-02 11:09:39 +0100

    Ajout application-test.properties et mise à jour pom.xml, properties et tests

--- student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,14 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // utilise application-test.properties
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+        // Vérifie que le contexte Spring se charge correctement
     }
-
 }
